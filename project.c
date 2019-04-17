ff4201054c3f18aa79708283e040491b084a9b95
manisharao9090 <49312772+manisharao9090@users.noreply.github.com>
2019-04-17 21:27:25 +0530

    Update project.c

--- project.c
+++ project.c
@@ -4,6 +4,7 @@
 #include<string.h>
 #include<unistd.h>
 sem_t d,p,mutex;
+int flag=0;
 char buffer[1000];
 void *consultDoctor(){
 	sem_wait(&p);
@@ -37,31 +38,54 @@ void *noteTreatment(){
 	sem_wait(&mutex);
 	
         //printf("Enter Patient Process to all detail\n");
-	printf("All Detail :-");
+	printf("\nPrescription :-\n");
 	sleep(1);
 	printf("%s",buffer);
+        
 	sem_post(&p);
         sem_post(&mutex);
+  
         
 }
 
-
 int main(){
-	sem_init(&d,0,0);
+	printf("************** Doctor Patient Management ************\n ");
+	pthread_t p1,p2,p3;
+        
+        int i=1;
+        
+        printf("Doctor Takes 20-30 min for one patient.He is in the clinic for 3 hrs.\nSo he can attend 5-6 patient around.\n\n");
+        //scanf("%d",n);        
+        
+        int m=1;
+        if(m<=5){
+        printf("Doctor can attend only 5 patient a day.\n");
+          while(i<=5){
+        if(m==5)
+        printf("\n--Last patient--\n\n");
+        printf("\npatient %d\n\n",i);
+        sem_init(&d,0,0);
 	sem_init(&p,0,1);
 	sem_init(&mutex,0,0);
 
-	pthread_t p1,p2,p3;
-	pthread_create(&p1,NULL,consultDoctor,NULL);
+	
+        pthread_create(&p1,NULL,consultDoctor,NULL);
 	pthread_join(p1,NULL);
 	
         pthread_create(&p2,NULL,treatPatient,NULL);
 	pthread_join(p2,NULL);
         pthread_create(&p3,NULL,noteTreatment,NULL);
 	pthread_join(p3,NULL);
+        i++;
+        
+
+}
+                }
+   
+
+        printf("Enough for today.\n\nPlease come tomorrow.\n---Thank you.---");
 	return 0;
 }
-
 
 
 
